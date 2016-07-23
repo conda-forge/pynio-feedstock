6ffc6b9290e71c0c4d6b61501ea5addac333d1c6
ocefpaf <ocefpaf@gmail.com>
2016-07-23 19:19:51 -0300

    Update to latest beta

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
-export LDFLAGS="-L${PREFIX}/lib"
-
-if [[ $(uname) == Darwin ]]; then
-export LDFLAGS="${LDFLAGS} -headerpad_max_install_names"
-fi
-
-export F2CLIBS=gfortran
-export NETCDF_PREFIX=${PREFIX}
 export HAS_NETCDF4=1
+export HAS_HDFEOS=1
+export HAS_HDFEOS5=1
 export HAS_GDAL=1
+export HAS_GRIB2=1
+export NETCDF_PREFIX=${PREFIX}
+export F2CLIBS=gfortran
+export HAS_SZIP=0
 export HAS_HDF4=1
 export HAS_HDF5=1
 export HAS_GDAL=1
-export HAS_SZIP=0
-export HAS_GRIB2=1
-export HAS_HDFEOS=1
-export HAS_HDFEOS5=1
 
-export CXXFLAGS="-fPIC"
-export CPPFLAGS="-I${PREFIX}/include"
-export CFLAGS="-I${PREFIX}/include"
+export CXXFLAGS="-fPIC $CXXFLAGS"
+export LDFLAGS="-L$PREFIX/lib $LDFLAGS"
+export CPPFLAGS="-I$PREFIX/include $CPPFLAGS"
+export CFLAGS="-D_BSD_SOURCE -D_XOPEN_SOURCE -I$PREFIX/include $CFLAGS"
+
+if [[ $(uname) == Darwin ]]; then
+  export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
+fi
 
 $PYTHON setup.py install
